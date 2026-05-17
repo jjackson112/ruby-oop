514d73e0794923b4e2c2a7a6142ec3d605ef095d
jjackson112 <jjackson11211@gmail.com>
2026-05-17 16:51:22 -0400

    if elsif else

--- first_script.rb
+++ first_script.rb
@@ -7,6 +7,10 @@ number = gets.chomp.to_i
 
 puts number + 5 * 2 - 4 / 2
 
-if number >= 20 
+if number >= 50 
   puts "Wow that's a big number!"
+elsif number <=50
+  puts "Hmmm, interesting"
+else 
+  puts "Wait, what?"
 end
