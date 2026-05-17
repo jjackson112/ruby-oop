ecadb4ebf92d3dc374ba47a1eeaea46ff0500526
jjackson112 <jjackson11211@gmail.com>
2026-05-17 17:39:53 -0400

    working on case statement for numbers 1-9

--- numerology.rb
+++ numerology.rb
@@ -16,3 +16,17 @@ if number > 9
   number = number[0].to_i + number[1].to_i
 end
 
+# single digit path
+case number
+when 1
+  puts "Your numerology number is #{number}. \nOne is the leader. The number one indicates the abiliry to stand alone, and is a strong vibration. Ruled by the Sun."
+when 2
+  puts "Your numerology number is #{number}. \nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+when 3
+  puts "Your numerology number is #{number}. \nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
+when 4
+  puts "Your numerology number is #{number}. \nThis is the worker. Practical, with a love of detail, Fours are trustworth, hardworking, and helpful. Ruled by Uranus."
+when 5
+  puts "Your numerology number is #{number}. \nThis is the freedom lover. The number five is an intellectual vibration. These are idea people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+when 6
+  puts "Your numerology number is #{number}. \nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
