0dfe7ddb1d43f2b6619ad30b31930aae27174b11
jjackson112 <jjackson11211@gmail.com>
2026-05-17 18:13:13 -0400

    always three method

--- first_method.rb
+++ first_method.rb
@@ -3,3 +3,14 @@ def my_method
 end
 
 my_method
+
+# ask user for number 
+# convert number to integer
+# Ruby does math - output
+def always_three
+  puts "Give a number"
+  user_number = gets.to_i
+  puts "Always " + (((user_number +5) *2 -4) / 2 - user_number).to_s
+end
+
+always_three
