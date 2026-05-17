0bd99983a4265f7e37613f4869517f0fb418fb49
jjackson112 <jjackson11211@gmail.com>
2026-05-17 17:52:11 -0400

    rm hidden newline - gets captures the user birthdate but it's not necessary to see

--- numerology.rb
+++ numerology.rb
@@ -1,7 +1,7 @@
 # numerology app project - rough draft
 puts "What's your birthdate? It must be in this format MMDDYYY"
 
-birthdate = gets
+birthdate = gets.chomp
 
 # use array syntax to access each element (number) in the birthdate, convert each one to an integer, and add them all together
 number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
