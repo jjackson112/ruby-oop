cfac6aa5874d362d375f71b9da7f4060e8706abb
jjackson112 <jjackson11211@gmail.com>
2026-05-17 22:38:55 -0400

    Fizzbuzz test

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+# FizzBuzz test 1-100
+# If divisible by 3, print Fizz
+# If divisible by 5, print Buzz
+# If divisible by both 3 and 5 print FizzBuzz
+# use modulo operator
+
+i = 0
+while i < 101
+  if i%3 == 0 && i%5 == 0 # divides evenly
+    puts "FizzBuzz"
+  elsif i%3 == 0
+    puts "Fizz"
+  elsif i%5 == 0
+    puts "Buzz"
+  else
+    number 
+  end
+end
