ba523a0725210822f91c79685a14374e5384309f
jjackson112 <jjackson11211@gmail.com>
2026-05-17 21:41:26 -0400

    annoying while loop

---
+++ loopy_ruby.rb
@@ -0,0 +1,10 @@
+# while loops keep going until the statement is FALSE
+# if else statements stop once the condition is met or TRUE
+
+puts "Do you find me annoying? (Y/N)"
+answer = gets.chomp.downcase
+
+while (answer = n)
+  puts "Do you find me annoying? (Y/N)"
+  answer = gets.chomp.downcase
+end
