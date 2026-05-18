74bacde50b17440ac3231bd502b7af0514281d21
jjackson112 <jjackson11211@gmail.com>
2026-05-17 21:07:56 -0400

    use number as argument and return meaning

--- numerology.rb
+++ numerology.rb
@@ -22,24 +22,27 @@ def get_birth_path_number
 end
 
 # single digit path
-case number
-when 1
-  puts "Your numerology number is #{number}. \nOne is the leader. The number one indicates the abiliry to stand alone, and is a strong vibration. Ruled by the Sun."
-when 2
-  puts "Your numerology number is #{number}. \nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
-when 3
-  puts "Your numerology number is #{number}. \nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
-when 4
-  puts "Your numerology number is #{number}. \nThis is the worker. Practical, with a love of detail, Fours are trustworth, hardworking, and helpful. Ruled by Uranus."
-when 5
-  puts "Your numerology number is #{number}. \nThis is the freedom lover. The number five is an intellectual vibration. These are idea people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
-when 6
-  puts "Your numerology number is #{number}. \nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
-when 7
-  puts "Your numerology number is #{number}. \nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
-when 8
-  puts "Your numerology number is #{number}. \nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
-when 9
-  puts "Your numerology number is #{number}. \nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
-else
-  puts "Uh oh! Your birth path number is not 1-9! But you have an interesting soul!"
+def get_message(number)
+  case number
+  when 1
+    puts "Your numerology number is #{number}. \nOne is the leader. The number one indicates the abiliry to stand alone, and is a strong vibration. Ruled by the Sun."
+  when 2
+    puts "Your numerology number is #{number}. \nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+  when 3
+    puts "Your numerology number is #{number}. \nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
+  when 4
+    puts "Your numerology number is #{number}. \nThis is the worker. Practical, with a love of detail, Fours are trustworth, hardworking, and helpful. Ruled by Uranus."
+  when 5
+    puts "Your numerology number is #{number}. \nThis is the freedom lover. The number five is an intellectual vibration. These are idea people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+  when 6
+    puts "Your numerology number is #{number}. \nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
+  when 7
+    puts "Your numerology number is #{number}. \nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
+  when 8
+    puts "Your numerology number is #{number}. \nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
+  when 9
+    puts "Your numerology number is #{number}. \nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+  else
+    puts "Uh oh! Your birth path number is not 1-9! But you have an interesting soul!"
+  end
+end
